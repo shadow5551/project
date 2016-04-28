594bf7abd14d7415b70e3f1be16b8fc69aca9955
dima <yanusikdima@mail.ru>
2016-04-28 14:35:18 +0300

    log In

---
+++ src/main/java/com/websystique/springsecurity/dao/UserDaoImpl.java
@@ -0,0 +1,22 @@
+package com.websystique.springsecurity.dao;
+
+import com.websystique.springsecurity.model.User;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+@Repository("userDao")
+public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
+
+    public User findById(int id) {
+        return getByKey(id);
+    }
+
+    public User findBySSO(String sso) {
+        Criteria crit = createEntityCriteria();
+        crit.add(Restrictions.eq("ssoId", sso));
+        return (User) crit.uniqueResult();
+    }
+
+
+}
